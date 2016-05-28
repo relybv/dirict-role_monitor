448bbed67b30a188d4e5c5a475b078d92f2c9ddf
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-28 14:28:07 +0000

    prepare rspec monitor for nagios

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,5 +11,7 @@ class role_monitor
 {
   # a role includes one or more profiles and at least a 'base' profile
   include ::profile_base
+#  include ::profile_base::rspec_monitor
   include ::profile_rsyslog
+#  include ::profile_rsyslog::rspec_monitor
 }
